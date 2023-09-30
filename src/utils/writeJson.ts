74a63eb2affddaef9bd14ba600383199876a983f
Pierre-François Salmand <maabaa@gmail.com>
2023-09-30 19:03:13 +0200

    Improve writeJson helper

--- src/utils/writeJson.ts
+++ src/utils/writeJson.ts
@@ -1,14 +1,20 @@
-import fs from 'fs';
-import path, { dirname } from 'path';
-import { fileURLToPath } from 'url';
+import fs from 'fs/promises';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-const dataPath = path.resolve(__dirname, '../data');
+import { resolvePath } from './resolvePath';
 
-export const writeJson = (data: object, filePath: string) => {
-  fs.writeFile(`${dataPath}/${filePath}`, JSON.stringify(data), (err) => {
-    if (err) throw err;
-    console.log(`JSON has been written to ${filePath}.`);
-  });
+const dataPath = resolvePath('../data');
+
+export const writeJson = async (dataObject: object, relativePath: string) => {
+  try {
+    await fs.access(dataPath);
+  } catch (err) {
+    console.log('Error accessing directory', err);
+  }
+
+  try {
+    await fs.writeFile(`${dataPath}/${relativePath}`, JSON.stringify(dataObject));
+    console.log(`JSON has been written to ${relativePath}.`);
+  } catch (err) {
+    console.error('Error writing file', err);
+  }
 };

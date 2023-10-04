33be49cbf7f7fb2596709facb20aad41599c6fb8
Pierre-François Salmand <maabaa@gmail.com>
2023-10-04 18:07:08 +0200

    Rework initial states

--- src/state/jotai.ts
+++ src/state/jotai.ts
@@ -1,9 +1,12 @@
+import { atom, getDefaultStore } from 'jotai';
 import { atomWithStorage } from 'jotai/utils';
 
 import grades from '../data/grades.json';
 
-const juniorMinRate = grades.Junior.minDailyRate;
+export const store = getDefaultStore();
 
 export const selectedExperience = atomWithStorage('selExperience', 0);
-export const selectedRate = atomWithStorage('selRate', juniorMinRate);
+export const selectedRate = atomWithStorage('selRate', grades.Junior.minDailyRate);
 export const selectedDays = atomWithStorage('selDays', 218);
+
+export const salaryResult = atom(0);

072da2244b57532b20b075e34dd9231aa2b2d128
Pierre-François Salmand <maabaa@gmail.com>
2023-10-10 17:35:59 +0200

    Write section 5 - Intermission

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import ADNTium from "./components/01adnTium";
 import Adventuriers from "./components/02adventuriers";
 import Renumeration from "./components/03renumeration";
 import Simulateur from "./components/04simulateur";
+import Intermission from "./components/05intermission";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Adventuriers />
       <Renumeration />
       <Simulateur />
+      <Intermission />
     </>
   );
 }

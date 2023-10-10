84b8333b6fc3139a9d471f859b1fffd79193880f
Pierre-François Salmand <maabaa@gmail.com>
2023-10-10 16:43:17 +0200

    Write section 1 - ADN Tium

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import "./tailwind.css";
 
+import ADNTium from "./components/01adnTium";
 import SalaryForm from "./components/SalaryForm";
 
 function App() {
   return (
     <>
+      <ADNTium />
       <SalaryForm />
     </>
   );
---
+++ src/components/01adnTium.tsx
@@ -0,0 +1,29 @@
+import clsx from "clsx";
+
+function ADNTium() {
+  return (
+    <>
+      <h2>ADN&nbsp;TIUM - Valoriser vos&nbsp;talents</h2>
+      <section className={clsx("flex flex-col gap-5", "p-5", "sm:p-10", "rounded-lg bg-slate-800")}>
+        <h3>
+          Notre mission "Valoriser les talents". C'est une mission humaine avant tout, avec 3
+          piliers :
+        </h3>
+        <ul>
+          <li>La montée en compétences individuelle et en équipe est l'ADN d'Adventium ;</li>
+          <li>
+            Une aventure collective dans un grand esprit d'équipe, accompagnée et soutenue par des
+            dirigeants qui sont aussi des opérationnels, ce qui permet à chacun de trouver sa place
+            ;
+          </li>
+          <li>
+            Une rémunération transparente, atypique et unique sur le marché, qui valorise le profil
+            en temps réel.
+          </li>
+        </ul>
+      </section>
+    </>
+  );
+}
+
+export default ADNTium;

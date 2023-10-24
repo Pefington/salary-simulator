fff7964624d07f1bd2d25faa6d486ae3677864d4
Pierre-François Salmand <maabaa@gmail.com>
2023-10-24 23:49:55 +0200

    Start implementing the salary target interface

--- src/components/04simulateur/SalaryResult.tsx
+++ src/components/04simulateur/SalaryResult.tsx
@@ -1,7 +1,15 @@
+import clsx from "clsx";
 import { useAtom, useAtomValue } from "jotai";
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 
-import { salaryResult, selectedDays, selectedExperience, selectedRate } from "../../state/jotai";
+import { EditIcon } from "../../icons";
+import {
+  lockSalary,
+  salaryResult,
+  selectedDays,
+  selectedExperience,
+  selectedRate,
+} from "../../state/jotai";
 import { getYearlyGrossSalary } from "../../utils/getSalary";
 
 function SalaryResult() {
@@ -9,7 +17,11 @@ function SalaryResult() {
   const rate = useAtomValue(selectedRate);
   const days = useAtomValue(selectedDays);
 
+  const [salaryLocked, setSalaryLocked] = useAtom(lockSalary);
   const [result, setResult] = useAtom(salaryResult);
+
+  const salaryInputRef = useRef<HTMLInputElement>(null);
+
   const roundedResult = Math.floor(result);
   const monthlyResult = Math.floor(result / 12);
 
@@ -18,10 +30,65 @@ function SalaryResult() {
     setResult(newSalary);
   }, [experience, rate, days, setResult]);
 
+  useEffect(() => {
+    if (salaryLocked) {
+      salaryInputRef.current?.select();
+    }
+  }, [salaryLocked]);
+
+  const handleLockSalary = () => {
+    setSalaryLocked(!salaryLocked);
+  };
+
   return (
     <div className="flex min-w-max flex-col md:pl-5">
       <h4>Rémunération annuelle brute&nbsp;:</h4>
-      <p className="pb-6 pt-2 text-2xl font-bold text-adv-gold">{`${roundedResult} €`}</p>
+      <div
+        className={clsx(
+          "flex items-baseline gap-4",
+          "max-w-min",
+          "[&>:not(svg)]:mb-6 [&>:not(svg)]:mt-2",
+          "",
+          "",
+        )}
+      >
+        {salaryLocked ? (
+          <>
+            <input
+              ref={salaryInputRef}
+              className={clsx(
+                "text-2xl font-bold text-adv-gold",
+                "max-w-[70px]",
+                "max-h-[40px]",
+                "bg-sky-950",
+                "",
+              )}
+              type="text"
+              value={roundedResult}
+              onChange={({ target }) => setResult(Number(target.value))}
+            />
+            <span className="-ml-[12.5px] text-2xl font-bold text-adv-gold">€</span>
+          </>
+        ) : (
+          <p className={clsx("text-2xl font-bold text-adv-gold", "", "", "", "", "")}>
+            {roundedResult}&nbsp;€
+          </p>
+        )}
+
+        <EditIcon
+          height="25px"
+          className={clsx(
+            "translate-y-[3px]",
+            "transition-opacity duration-500 ease-in-out",
+            salaryLocked ? "fill-red-500" : "fill-adv-gold/50",
+            "hover:cursor-pointer hover:fill-adv-gold/100",
+            "",
+            "",
+            "",
+          )}
+          onClick={handleLockSalary}
+        />
+      </div>
       <h4>Rémunération mensuelle brute&nbsp;:</h4>
       <p className="pb-6 pt-2 text-2xl font-bold text-adv-gold">{`${monthlyResult} €`}</p>
       <p className="pb-2 text-lg">Cette rémunération inclut&nbsp;:</p>

cc3660564e14b8a3a40401fd1f4869ac0e1f978a
Pierre-François Salmand <maabaa@gmail.com>
2023-11-08 18:40:39 +0100

    Add bullseye icon

--- src/icons.tsx
+++ src/icons.tsx
@@ -8,10 +8,11 @@ Copyright 2023 Fonticons, Inc.
 interface EditIconProps {
   height?: string;
   className?: string;
+  title?: string;
   onClick?: () => void;
 }
 
-export const EditIcon = ({ height, className, onClick }: EditIconProps) => {
+export const BullsEyeIcon = ({ height, className, title, onClick }: EditIconProps) => {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
@@ -20,7 +21,8 @@ export const EditIcon = ({ height, className, onClick }: EditIconProps) => {
       viewBox="0 0 512 512"
       onClick={onClick}
     >
-      <path d="M441 58.9L453.1 71c9.4 9.4 9.4 24.6 0 33.9L424 134.1 377.9 88 407 58.9c9.4-9.4 24.6-9.4 33.9 0zM209.8 256.2L344 121.9 390.1 168 255.8 302.2c-2.9 2.9-6.5 5-10.4 6.1l-58.5 16.7 16.7-58.5c1.1-3.9 3.2-7.5 6.1-10.4zM373.1 25L175.8 222.2c-8.7 8.7-15 19.4-18.3 31.1l-28.6 100c-2.4 8.4-.1 17.4 6.1 23.6s15.2 8.5 23.6 6.1l100-28.6c11.8-3.4 22.5-9.7 31.1-18.3L487 138.9c28.1-28.1 28.1-73.7 0-101.8L474.9 25C446.8-3.1 401.2-3.1 373.1 25zM88 64C39.4 64 0 103.4 0 152V424c0 48.6 39.4 88 88 88H360c48.6 0 88-39.4 88-88V312c0-13.3-10.7-24-24-24s-24 10.7-24 24V424c0 22.1-17.9 40-40 40H88c-22.1 0-40-17.9-40-40V152c0-22.1 17.9-40 40-40H200c13.3 0 24-10.7 24-24s-10.7-24-24-24H88z" />
+      <title>{title}</title>
+      <path d="M448 256A192 192 0 1 0 64 256a192 192 0 1 0 384 0zM0 256a256 256 0 1 1 512 0A256 256 0 1 1 0 256zm256 80a80 80 0 1 0 0-160 80 80 0 1 0 0 160zm0-224a144 144 0 1 1 0 288 144 144 0 1 1 0-288zM224 256a32 32 0 1 1 64 0 32 32 0 1 1 -64 0z" />
     </svg>
   );
 };

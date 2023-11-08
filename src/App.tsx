6d3b5c3b1b9b048d902453bb08295ce298f256f4
Pierre-François Salmand <maabaa@gmail.com>
2023-11-08 18:44:35 +0100

    WIP on CSS

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,23 @@
 import "./tailwind.css";
 
-import ADNTium from "./components/01adnTium";
-import Adventuriers from "./components/02adventuriers";
-import Renumeration from "./components/03renumeration";
+// import ADNTium from "./components/01adnTium";
+// import Adventuriers from "./components/02adventuriers";
+// import Renumeration from "./components/03renumeration";
 import Simulateur from "./components/04simulateur";
-import Intermission from "./components/05intermission";
-import Profils from "./components/06profils";
-import Contact from "./components/07contact";
+// import Intermission from "./components/05intermission";
+// import Profils from "./components/06profils";
+// import Contact from "./components/07contact";
 
 function App() {
   return (
     <>
-      <ADNTium />
-      <Adventuriers />
-      <Renumeration />
+      {/* <ADNTium /> */}
+      {/* <Adventuriers /> */}
+      {/* <Renumeration /> */}
       <Simulateur />
-      <Intermission />
-      <Profils />
-      <Contact />
+      {/* <Intermission /> */}
+      {/* <Profils /> */}
+      {/* <Contact /> */}
     </>
   );
 }
